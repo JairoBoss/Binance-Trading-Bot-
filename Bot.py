ac96f2ee2cb45e98bfdd8e158269ddfadd68b213
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-05-06 11:19:39 -0500

    U.U

---
+++ Bot.py
@@ -0,0 +1,74 @@
+import config
+from binance.client import Client
+from binance.enums import *
+import time
+import datetime
+import numpy as np
+
+client = Client(config.API_KEY, config.API_SECRET, tld='com')
+symbolTicker = 'BTCUSDT'
+precioAnterior = 0
+quantityOrders = 400
+compra = False
+
+
+#Encontrar el valor de la moneda
+list_of_tickers = client.get_all_tickers()
+for tick_2 in list_of_tickers:
+    if tick_2["symbol"] == symbolTicker:
+        precioAnterior = float(tick_2["price"])
+
+print ("*** Compra inicial ***")
+print("Se compro a: ", precioAnterior)
+order = client.order_market_buy(
+    symbol = symbolTicker,
+    quantity = quantityOrders
+)
+
+
+while 1:
+    time.sleep(3)
+    #Encontrar el valor de la moneda
+    list_of_tickers = client.get_all_tickers()
+    for tick_2 in list_of_tickers:
+        if tick_2["symbol"] == symbolTicker:
+            precioActual = float(tick_2["price"])
+
+    print("***********")
+    #print("Se desea comprar a: ", (precioAnterior+(precioAnterior*.001)))    
+    if ((precioAnterior+(precioAnterior*.001)) < (precioActual)):
+        print("Se vendio a: ", precioActual) 
+        order = client.order_market_sell(
+            symbol = symbolTicker,
+            quantity = 0.00022
+        )
+        compra = True
+        precioAnterior = precioActual
+        
+        time.sleep(15)
+
+        while 1:
+            
+            list_of_tickers = client.get_all_tickers()
+            for tick_2 in list_of_tickers:
+                if tick_2["symbol"] == symbolTicker:
+                    precioActual = float(tick_2["price"])
+
+            print(".---------------------------------------------------------------------.")
+            print("Buscando un precio optimo para comprar, el precio actual es de:", precioActual)
+            
+            if (precioActual < ((precioActual*.001)+precioActual)):
+                print("Se compra a: ", precioActual)
+                order = client.order_market_buy(
+                symbol = symbolTicker,
+                quantity = quantityOrders
+                )
+                
+                break
+
+            #time.sleep(3)
+                
+
+    else:
+        print("El precio es menor, paso")
+        print("El precio es de: ", precioActual)
