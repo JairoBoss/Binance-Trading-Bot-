8d37ed17575291b492dcdcd167ce24a83ef92659
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-05-12 10:03:35 -0500

    Create stopLossDinamico.py

---
+++ stopLossDinamico.py
@@ -0,0 +1,3 @@
+#Perseguira el precio mas chico, para comprarlo en el primer rebote
+#Despues de haber comprado pondra dos limites, inferior y superior 
+#Si llega a superar esos limites tendra que vender, limite inferior .5% abajo & 1% superior
