7645bb200f2bee21df10cd987d80aaf4c992f7d5
Daniel <dani_wii20@hotmail.com>
2021-05-22 13:33:35 -0500

    Se añadieron funciones max y min, también nuevas tácticas de compra

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from binance.enums import *
 import time
 import datetime
 import numpy as np
+import winsound as ws
 
 client = Client(config.API_KEY, config.SECRET, tld='com')
 symbolTicker = 'BNBUSDT'
@@ -12,16 +13,22 @@ precioCompra = 0
 quantityOrders = 0
 compra = False
 ganancia = 0
-inversion = 1000
+inversion = 50000
 porcentaje_comision = .00075
 
 
-
 while 1:
-    klines = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_5MINUTE)
+
+    time.sleep(1)
+
+    klines = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_1MINUTE)
     klines_h = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_1MINUTE)
 
+    min = func.min(klines)
+    max = func.max(klines)
+
     media_min = func.media_min_klines(klines_h)
+    media_max = func.media_max_klines(klines_h)
 
     # Encontrar el valor de la moneda
     list_of_tickers = client.get_all_tickers()
@@ -29,23 +36,33 @@ while 1:
         if tick_2["symbol"] == symbolTicker:
             precioCompra = float(tick_2["price"])
 
-    if ((precioCompra <= float(klines[len(klines)-1][3]) and (precioCompra <= media_min))):  
+    if ((precioCompra >= max*.991) and ((precioCompra >= ((media_max+max)/2)*.991))): 
+        '(precioCompra <= ((media_min+min)/2)*1.007) or '
         'or ((precioCompra <= (float(klines[len(klines)-1][3]))) + (float(klines[len(klines)-1][3])*.01)'
-        print("*** Compra inicial ***")
-        print("Se compro a: ", precioCompra)
-        # order = client.order_market_buy(
-        #    symbol = symbolTicker,
-        #   quantity = quantityOrders
-        # )
-        break
+
+        if ((precioCompra >= float(klines[len(klines)-1][2])) and ()):
+
+            ws.MessageBeep(type=1)
+            print("*** Compra inicial ***")
+            print("Se compro a: ", precioCompra)
+            # order = client.order_market_buy(
+            #    symbol = symbolTicker,
+            #   quantity = quantityOrders
+            # )
+            break
+        
     else:
         print("\nPrecio actual",precioCompra)
-        print("\nPrecio mínimo de 5 min",float(klines[len(klines)-1][3]))
-        print("\nMedia de precio mínimo de 30 mins", media_min)
+        print("\nPrecio máximo de 1hr min",max)
+        print("\nPrecio mínimo de 1hr min",min)
+        print("\nMedia de precio máximo de 30 mins", media_max)
+        print("\nMedia de precio mínimo de 30 mins", media_min)        
+        print("\nEsa wea max", ((media_max+max)/2)*.993)
+        print("\nEsa wea min", ((media_min+min)/2)*1.007)
         print("\nbuscando precio..")
 
 while 1:
-    time.sleep(3)
+    time.sleep(1)
     # Encontrar el valor de la moneda
     list_of_tickers = client.get_all_tickers()
     for tick_2 in list_of_tickers:
@@ -54,11 +71,7 @@ while 1:
 
     klines = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_5MINUTE)        
 
-    maximo = klines[len(klines)-1][2]
-    minimo = klines[len(klines)-1][3]
-
-    print("Máximo: ", maximo)
-    print("Mínimo: ", minimo)
+    
 
     comisionCompra = (inversion*porcentaje_comision)
     comisionVenta = (inversion+((precioActual-precioCompra) /precioActual))*porcentaje_comision
@@ -70,8 +83,9 @@ while 1:
 
     print("Ganancia tiempo real:", gananciaTR)
 
-    if (((comisionCompra+comisionVenta) < ((precioActual - precioCompra)*(inversion/precioCompra)))):
+    if (((comisionCompra+comisionVenta) < ((precioActual - precioCompra)*(inversion/precioCompra))) or (gananciaTR <= (-inversion*.02))):
         print("Se vendio a: ", precioActual)
+        ws.MessageBeep(type=1)
         ganancia += (precioActual - precioCompra)*(inversion / precioCompra)-comisionCompra-comisionVenta
         # order = client.order_market_sell(
         #    symbol = symbolTicker,
@@ -81,15 +95,18 @@ while 1:
         compra = True
         precioCompra = precioActual
 
-        time.sleep(15)
+        time.sleep(2)
 
         while 1:
-            klines = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_5MINUTE) 
-
+            klines = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_1MINUTE) 
             
             klines_h = client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_1MINUTE)
-    
+
+            min = func.min(klines)
+            max = func.max(klines)
+
             media_min = func.media_min_klines(klines_h)
+            media_max = func.media_max_klines(klines_h)
 
             list_of_tickers = client.get_all_tickers()
             for tick_2 in list_of_tickers:
@@ -97,12 +114,17 @@ while 1:
                     precioCompra = float(tick_2["price"])
 
             print("\nPrecio actual",precioCompra)
-            print("\nPrecio mínimo de 5 min",float(klines[len(klines)-1][3]))
-            print("\nMedia de precio mínimo de 30 mins", media_min)
-            print("\nbuscando precio..")  
+            print("\nPrecio máximo de 1hr min",max)
+            print("\nPrecio mínimo de 1hr min",min)
+            print("\nMedia de precio máximo de 1 hr", media_max)
+            print("\nMedia de precio mínimo de 1 hr", media_min)        
+            print("\nEsa wea max", ((media_max+max)/2)*.993)
+            print("\nEsa wea min", ((media_min+min)/2)*1.007)
+            print("\nbuscando precio..")
 
-            if ((precioCompra <= float(klines[len(klines)-1][3]) and (precioCompra <= media_min))):
+            if ((precioCompra >= max*.991) and ( (precioCompra >= ((media_max+max)/2)*.991))):
                 print("Se compra a: ", precioCompra)
+                ws.MessageBeep(type=1)
                 # order = client.order_market_buy(
                 #symbol = symbolTicker,
                 #quantity = quantityOrders
@@ -110,7 +132,7 @@ while 1:
 
                 break
 
-            # time.sleep(3)
+            time.sleep(1)
 
     else:
         print("El precio es menor, paso")
--- func.py
+++ func.py
@@ -1,15 +1,31 @@
 import numpy as np
 
 def media_max_klines(klines):
-    suma = 0
-    for i in range(len(klines)):
-        suma = suma + float(klines[i][2])  
-    media = suma / len(klines)
+    suma = 0     
+    for i in range(10):        
+        suma = suma + float(klines[len(klines)-(i+1)][2])  
+    media = suma / 10
     return media
 
 def media_min_klines(klines):
-    suma = 0  
-    for i in range(len(klines)):
-        suma = suma + float(klines[i][3])  
-    media = suma / len(klines)
+    suma = 0     
+    for i in range(10):        
+        suma = suma + float(klines[len(klines)-(i+1)][3])  
+    media = suma / 10
     return media
+
+def min(klines):
+    min = 0
+    for i in range(10):
+        piv = float(klines[len(klines)-(i+1)][3])
+        if ((piv <= min) or (min == 0)):
+            min = piv
+    return min
+    
+def max(klines):
+    max = 0
+    for i in range(10):
+        piv = float(klines[len(klines)-(i+1)][2])
+        if ((piv >= max) or (max == 0)):
+            max = piv
+    return max
