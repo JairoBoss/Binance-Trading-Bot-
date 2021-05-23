7f8c95c95ea2e104c8c290b789cf7a52f5fbeea5
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-05-22 19:14:28 -0500

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -1,15 +1,40 @@
 import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
 
 
 #Ventana raiz Java equivalente al JFrame
-root = tk.Tk()
+root = tk.Tk(className='Binance Trading bot')
+root.geometry("1000x310")
+
+#img = Image.open("Grafica.png")
+#img = img.resize((470, 275))
+#795 30
+#
+#tkimage = ImageTk.PhotoImage(img)
+#tk.Label(root, image=tkimage).grid()
+
+var = StringVar()
+var.set('GANANCIASSSSSSSSSSS')
 
 #Creando el Label, equivalente al JLabel
 
-lblGanacias = tk.Label(root, text ='Ganancias')
+lblGanacias = tk.Label(root, text =var)
+
 lblPerdida = tk.Label(root, text ='Perdida')
+
+lblUltimasOperacopmes = tk.Label(root, text ='Ultimas operaciones')
 #Esto es para poner la posicion
-lblGanacias.place(relx = .1, rely = .1)
-lblPerdida.place(relx = .3, rely = .3)
-# Poner visible Frame, equivalente al .setVisible(true)
+lblGanacias.place(relx = .560, rely = .1090)
+lblPerdida.place(relx = .560, rely = .5272)
+lblUltimasOperacopmes.place(relx = .795, rely = .1090)
+
+
+
 root.mainloop()
+
+    
+
+# Poner visible Frame, equivalente al .setVisible(true)
+
+
